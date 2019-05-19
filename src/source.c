e3814a9484269bd6772d7c6e74c5b3896199a28c
S.Sabev <526473+gb96@users.noreply.github.com>
2019-05-19 13:07:20 +0200

    1-6. Verify that the expression get char()

--- src/source.c
+++ src/source.c
@@ -1,28 +1,20 @@
 /* 
-Exercise 1-3, 1-4. Print Fahrenheit-Celsius table
+Exercise 1-6. Verify that the expression get char() 
+ EDF is 0 or 1
 v3 float
 */
 #include <stdio.h>
 
 int main()
 {
-    float fahr, celsius;
-    float lower, upper, step;
-
-    lower = 0.f;
-    upper = 300.f;
-    step = 20.f;
-
-        printf("Fahr   Celsius\n");
-        printf("-------------\n");
-
-    for(celsius = lower; celsius <= upper; celsius +=step)
+    int c;
+    
+    while((c = getchar()) != EOF)
     {
-        fahr = 32.f + (9.0f*celsius)/5.0f;
-        printf("%6.1f %8.1f\n", fahr, celsius);
+        printf("%d\n",c);
     }
 
+    printf("\n%d - at EOF\n", c);
     getchar();
-    
     return 0; 
 }
